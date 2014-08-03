b102305fb5df695f087562c7d3a879c695de7d86
gabriel <gabriel@gabriel-Lenovo.(none)>
2014-08-03 17:41:50 -0300

    checkCollision

--- ProjetoCOO/src/entities/drawer/body/basic_body/Body.java
+++ ProjetoCOO/src/entities/drawer/body/basic_body/Body.java
@@ -1,50 +1,56 @@
 package entities.drawer.body.basic_body;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import entities.states.InactiveState;
 import interfaces.States;
 
-public class Body {
+public abstract class Body{
 
 	private States currentState;
-	private double posX, posY, velX, velY;								
+	private Posicao pos;
+	private Velocidade vel;								
+	private double raio;
 	
 	public Body(double posX, double posY, double velX, double velY) {
-		this.posX = posX;
-		this.posY = posY;
-		this.velX = velX;
-		this.velY = velY;
+		this.pos = new Posicao(posX, posY);
+		this.vel = new Velocidade(velX, velY);
 		this.currentState = new InactiveState();
 	}
 	
-	/*Getters e Setters*/
-	public double getPosX() {
-		return posX;
+	public Posicao getPosicao() {
+		return pos;
 	}
-	public void setPosX(double posX) {
-		this.posX = posX;
+	
+	public Velocidade getVelocidade() {
+		return vel;
 	}
-	public double getPosY() {
-		return posY;
+	
+	public double getRaio() {
+		return raio;
 	}
-	public void setPosY(double posY) {
-		this.posY = posY;
-	}
-	public double getVelX() {
-		return velX;
-	}
-	public void setVelX(double velX) {
-		this.velX = velX;
-	}
-	public double getVelY() {
-		return velY;
-	}
-	public void setVelY(double velY) {
-		this.velY = velY;
-	}	
 	public States getCurrentState() {
 		return currentState;
 	}
+	
 	public void setCurrentState(States currentState) {
 		this.currentState = currentState;
 	}
+	
+	private static boolean checkColission(Body b1, Body b2){
+		if(b1.pos.distancia(b2.pos) < (b1.raio+b2.raio)*0.8){
+		//	b2.onColide(b1);
+		//	b1.onColide(b2);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkColission(Body other){
+		return Body.checkColission(this ,other);
+	}
+	
+	//public abstract void onColide(Body anotherBody);
+	
 }
